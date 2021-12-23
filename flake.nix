2a35b664425fd6b139fa9fb55b998e6f23e7ea99
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2021-12-23 21:34:59 +0200

    Init

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Portcast Assignmement";
+
+  inputs = {
+    nixpkgs.url = "github:yevhenshymotiuk/nixpkgs/release-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = with pkgs; mkShell { buildInputs = [ poetry python37 ]; };
+      });
+}
