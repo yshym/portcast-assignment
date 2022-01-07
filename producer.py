0735045a4eb0eb13124d7e45da20025718a30abc
Yevhen Shymotiuk <yevhenshymotiuk@pm.me>
2022-01-07 17:58:22 +0200

    producer: use prefect for scheduled tasks

--- producer.py
+++ producer.py
@@ -1,24 +1,40 @@
-from time import sleep
+from collections import deque
+from datetime import timedelta
 
+import prefect
+from prefect import Flow
+from prefect.schedules import IntervalSchedule
 from redis_ import redis
 from redis_.queue_ import TaskQueue
 from redis_.task import Task
 
 
-if __name__ == "__main__":
-    numbers = [
-        # containers
-        "TGBU5600894",
-        "TRHU5131609",
-        # bills of lading
-        "MEDUJ1656290",
-        "MEDUJ1656241",
-        "MEDUM5024051",
-    ]
+numbers = deque([
+    # containers
+    "TGBU5600894",
+    "TRHU5131609",
+    # bills of lading
+    "MEDUJ1656290",
+    "MEDUJ1656241",
+    "MEDUM5024051",
+])
 
-    for number in numbers:
-        task_queue = TaskQueue("tasks", redis)
-        task = Task("scrape-shipment", number)
-        task_queue.push(task)
-        print(f"Added '{task}' to the queue")
-        sleep(4)
+
+@prefect.task
+def run():
+    global numbers
+    if not numbers:
+        print("Numbers list is empty")
+        return
+    number = numbers.popleft()
+    task_queue = TaskQueue("tasks", redis)
+    task = Task("scrape-shipment", number)
+    task_queue.push(task)
+    print(f"Added '{task}' to the queue")
+
+
+if __name__ == "__main__":
+    schedule = IntervalSchedule(interval=timedelta(seconds=15))
+    with Flow("Producer", schedule) as flow:
+        run()
+    flow.run()
